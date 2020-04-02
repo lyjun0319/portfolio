a6720c1dc90d06fa7e45a7c9edd98328d7422468
lyjun0319 <lyjun0319@gmail.com>
2020-04-02 10:04:03 +0900

    재사용코드 변경

--- inc/js/main.js
+++ inc/js/main.js
@@ -1,96 +1,115 @@
-/* ===== Portfolio ===== */
-$.ajax({
-    type: "post",
-    url: "/inc/json/portfolio.json",
-    dataType: "JSON",
-    error: function(){
-        alert('통신실패');
-    },
-    success : function(data){
-        const list = data;
-        $.each(list, function(key, value){
-            let _listHtml = listHtml(value);
-            listInnerHtml(_listHtml);
-        });
-    }
-});
+const portfolioUrl = "/inc/json/portfolio.json";
+const historyUrl = "/inc/json/history.json";
 
-function listHtml(item){
-    let html = '';
-        html += "<article class='listItem'>";
-        html += "<h3 class='listTitle'>"+item.name+"</h3>";
-        html += "<div class='guideTxt'>";
-        html += arrWork(item.guide);
-        html += "</div>";
-        html += "<p class='useSkill'>"+item.skils+"</p>"
-        html += "<div class='extra_txt'>"
-        html += "<a href='"+item.linkUrl+"' class='linkUrl' target='_blank'>Link"
-        html += "</a>"
-        html += "<p class='listDay'>"+item.day+"</p>";
-        html += "</div>"
-        html += "</article>";
-    return html;
-};
+const arrPortfolio = dataParsing(portfolioUrl);
+const arrHistory = dataParsing(historyUrl);
 
-function listInnerHtml(item){
-    let appendTarget = document.getElementById('portfolio_list');
-    appendTarget.innerHTML += item;
-};
+mainPortfolioList(arrPortfolio);
+mainHistoryList(arrHistory);
+
+$(document).on('click', '.cp_Portfolio', function(e){
+    clearList();
+})
+
+//PORTFOLIO & HISTORY AJAX RETURN
+function dataParsing(links){
+    let newData = new Array();
+    $.ajax({
+        type: "post",
+        url: links,
+        async: false,
+        dataType: "JSON",
+        success : function(data){
+            newData = data;
+        },
+        error: function(){
+            alert('통신실패');
+        }
+    });
+    return newData;
+}
+
+// MAIN LOAD PORTFOLIO LIST
+function mainPortfolioList(data){
+    const obj = data;
+    const listType = "portfolio_list";
+    $.each(obj, function(key, value){
+        let _listHtml = portfolioHtml(value);
+        appendListDate(_listHtml, listType);
+    });
+}
 
 
-/* ===== Profile ===== */
-$.ajax({
-    type: "post",
-    url: "/inc/json/history.json",
-    dataType: "JSON",
-    error: function(){
-        alert('history 통신실패');
-    },
-    success : function(data){
-        const obj = data;
-        const companyHistory = obj.companyHistory;
-        const myHistory = obj.myHistory;
-        companyDate(companyHistory);
-    }
-});
+// MAIN LOAD HISTORY LIST
+function mainHistoryList(data){
+    const obj = data;
+    const companyHistory = obj.companyHistory;
+    const myHistory = obj.myHistory;
+    const listType = "cp_historyList"
+    companyDate(companyHistory,listType);
+}
 
-function companyDate(arr){
+function companyDate(arr, listType){
     $.each(arr, function(key, value){
         const htmls = companyHtml(value);
-        appendCompanyList(htmls)
-    })
-}
+        appendListDate(htmls, listType);
+    });
+};
 
+// PORTFOLIO HTML
+function portfolioHtml(item){
+    let html = '';
+    html += "<article class='listItem'>";
+    html += "<h3 class='listTitle'>"+item.name+"</h3>";
+    html += "<div class='guideTxt'>";
+    html += arrWork(item.guide, "portfolio");
+    html += "</div>";
+    html += "<p class='useSkill'>"+item.skils+"</p>";
+    html += "<div class='extra_txt'>";
+    html += "<a href='"+item.linkUrl+"'class='linkUrl' target='_blank'>Link</a>";
+    html += "<p class='listDay'>"+item.day+"</p>";
+    html += "</div>";
+    html += "</article>";
+    return html;
+};
+
+// COMPANY HTML
 function companyHtml(item){
-    
     let html = '';
     html += "<article class='company_list'>";
-    html += "<a href='"+item.link+"'>PORJECT VIEW</a>"
-    html += "<div class='list_head'>"
+    html += "<a href='"+item.link+"'>PORJECT VIEW</a>";
+    html += "<div class='list_head'>";
     html += "<h3 class='cp_name'>"+item.companyName+"</h3>";
     html += "<p class='cp_workday'>"+item.workDay+"</p>";
-    html += "</div>"
+    html += "</div>";
     html += "<p class='cp_skils'>"+item.skils+"</p>";
-    html += "<p class='cp_intro'>"+item.intro+"</p>"
-    html += "<p class='cp_choose'>"+item.reason+"</p>"
-    html += "<div class='work_intro'>"
-    html += arrWork(item.work);
-    html += "</div>"
+    html += "<p class='cp_intro'>"+item.intro+"</p>";
+    html += "<p class='cp_choose'>"+item.reason+"</p>";
+    html += "<div class='work_intro'>";
+    html += arrWork(item.work, "company");
+    html += "</div>";
     html += "</article>";
-
-    console.log(html)
     return html;
-}
+};
 
-function arrWork(arr){
+function arrWork(arr, type){
     let workHtml ='';
     $.each(arr, function(key, value){
-        workHtml += "<p> <span class='extra_dot'></span><span>"+value+"</span></p>";
+        workHtml += "<p><span class='extra_dot'></span><span>"+value+"</span></p>";
     });
+    type == "company" ? 
+        workHtml += "<button type='button' class='cp_Portfolio'>Portfolio View</button>" : undefined;
     return workHtml;
-}
+};
 
-function appendCompanyList(item){
-    let appendTarget = document.getElementById('cp_historyList');
+function appendListDate(item, listType){
+    const appendTarget = document.getElementById(listType);
     appendTarget.innerHTML += item;
 };
+
+function clearList(){
+    const portfolioTarget = document.getElementById('portfolio_list');
+    portfolioTarget.innerHTML = '';
+}
+
+
